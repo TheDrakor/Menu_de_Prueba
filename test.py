73499aec357b69a90e01d45f52f39281d2deb4e2
Lolo <chimichuymx@gmail.com>
2022-07-12 11:54:43 -0700

    Se han agregado espacios en test.py

--- test.py
+++ test.py
@@ -4,21 +4,23 @@ print("\n1.-Suma de 2 numeros")
 print("\n2.-Sacar el area de un triangulo")
 print("\n3.-Numero negativo o positivo")
 
-opcion = ''
+op = ''
 
-while opcion != "S":
+while op != "S":
 	try:
-		opcion = input("\n¿Que opcion eliges(presiona 's' para salir)?: ").upper()
+		op = input("\n¿Que opcion eliges(presiona 's' para salir)?: ").upper()
 
-		if int(opcion) == 1:
+		if int(op) == 1:
 			n1 = float(input("Dame un numero: "))
 			n2 = float(input("Dame otro numero: "))
 			print(n1+n2)
-		elif int(opcion)==2:
+
+		elif int(op)==2:
 			base = float(input("Base ->"))
 			altura = float(input("Altura ->"))
 			print((base *altura)/2)
-		elif int(opcion)==3:
+
+		elif int(op)==3:
 			numero = float(input("Dame un numero: "))
 			if numero>0:
 				print("Es un numero positivo")
@@ -26,6 +28,8 @@ while opcion != "S":
 				print("Es un numero negativo")
 			else:
 				print("El numero es cero")
+
 	except ValueError:
 		print("\nNo se permiten esos caracteres")
+		
 print("Gracias por participar!")
